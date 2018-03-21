cd65d487a7362545526a75b6a5b1b49764625d29
EnkeyMC <enkeymc@gmail.com>
2018-03-21 15:16:39 +0100

    Editorconfig, added .jar to build.xml, ignore out/

--- src/ija/project/BlockEditor.java
+++ src/ija/project/BlockEditor.java
@@ -10,21 +10,21 @@ import java.io.IOException;
 
 public class BlockEditor extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-    @Override
-    public void start(Stage primaryStage) {
-    	Parent root = null;
-    	try {
+	@Override
+	public void start(Stage primaryStage) {
+		Parent root = null;
+		try {
 			root = FXMLLoader.load(getClass().getResource("ui/fxml/MainPanel.fxml"));
 		} catch (IOException e) {
-    		System.err.println(e.getMessage());
-    		System.exit(1);
+			System.err.println(e.getMessage());
+			System.exit(1);
 		}
 		primaryStage.setTitle("Hello World");
 		primaryStage.setScene(new Scene(root, 300, 275));
 		primaryStage.show();
-    }
+	}
 }
