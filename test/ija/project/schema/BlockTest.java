750736e10ac7996d262daa1c66ef7829784d6303
petrHendrych <petr.hendrych97@seznam.cz>
2018-04-08 21:02:53 +0200

    Tested add input/output port

---
+++ test/ija/project/schema/BlockTest.java
@@ -0,0 +1,40 @@
+package ija.project.schema;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BlockTest {
+
+	@Test
+	public void addInputPort() {
+		Block b = new Block();
+		Type t = new Type();
+		b.addInputPort("inputPort", t);
+		boolean found = false;
+
+		for (BlockPort bp : b.getInputPorts()) {
+			if (bp.getPort().equals("inputPort")) {
+				found = true;
+				break;
+			}
+		}
+		assertTrue(found);
+	}
+
+	@Test
+	public void addOutputPort() {
+		Block b = new Block();
+		Type t = new Type();
+		b.addOutputPort("outputPort", t);
+		boolean found = false;
+
+		for (BlockPort bp : b.getOutputPorts()) {
+			if (bp.getPort().equals("outputPort")) {
+				found = true;
+				break;
+			}
+		}
+		assertTrue(found);
+	}
+}
