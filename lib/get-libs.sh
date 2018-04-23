6faf0172d0e346dd008824bf5ce7916c889f5031
Zdenek Chovanec <xchova19@stud.fit.vutbr.cz>
2018-04-23 15:04:38 +0200

    Always download to lib directory

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -2,6 +2,11 @@
 
 ANTLR="http://www.antlr.org/download/antlr-4.7.1-complete.jar"
 
+dir=`echo $0 | rev | cut -d"/" -f2- | rev`
+if [ "$dir" != "$0" ]; then
+	cd "$dir"
+fi
+
 if ! wget $ANTLR; then
 	echo "ERROR: Cannot download library ANTLR" >&2
 	exit 1
