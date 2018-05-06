f5d6854a66040bd371871a583cbec70058a100bb
Zdenek Chovanec <xchova19@stud.fit.vutbr.cz>
2018-05-06 22:56:46 +0200

    Fix typo in get-libs

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ANTLR="http://www.antlr.org/download/antlr-4.7.1-complete.jar"
-JUINT="http://central.maven.org/maven2/junit/junit/4.12/junit-4.12.jar"
+JUNIT="http://central.maven.org/maven2/junit/junit/4.12/junit-4.12.jar"
 HAMCREST="http://central.maven.org/maven2/org/hamcrest/hamcrest-all/1.3/hamcrest-all-1.3.jar"
 
 dir=`echo $0 | rev | cut -d"/" -f2- | rev`
